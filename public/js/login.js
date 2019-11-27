9b642072667ec252072e95962b4f4e3049c1fbec
Tuan Vinh <vinh.nguyentuan@dallmeier.com>
2019-11-27 18:05:32 +0700

    commit test

--- public/js/login.js
+++ public/js/login.js
@@ -7,27 +7,15 @@ function validateForm() {
     if ((un == username) && (pw == password)) {
 
         var xmlhttp = new XMLHttpRequest();
-        var url = "/api/user/login";
-        xmlhttp.open("POST", url, true);
+        var theUrl = "/api/user/login";
+        xmlhttp.open("POST", theUrl, false);
         xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.onreadystatechange = function (response) {
-            console.log(xmlhttp.status);
-            console.log(xmlhttp.statusText);
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                myFunction(xmlhttp.responseText);
-            }
-        };
         xmlhttp.send(JSON.stringify({ "email": "initUser@gmail.com", "password": "initUser" }));
 
-        return true;
+        // return true;
     }
     else {
         alert("Login was unsuccessful, please check your username and password");
         return false;
     }
-}
-
-function loginUser(usr, psw) {
-    xhttp.open("POST", "http://localhost:3000/api/user/login", true);
-    xhttp.send();
 }
