94096db3ba5d66c2fd700b6b3b13b2075c71e676
Tuan Vinh <vinh.nguyentuan@dallmeier.com>
2019-11-28 15:53:29 +0700

    login with api successfuly

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,22 @@
 function validateForm() {
     var un = document.loginform.uname.value;
     var pw = document.loginform.psw.value;
-    var username = "username";
-    var password = "username";
-    if ((un == username) && (pw == password)) {
 
-        var xmlhttp = new XMLHttpRequest();
-        var theUrl = "/api/user/login";
-        xmlhttp.open("POST", theUrl, false);
-        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.send(JSON.stringify({ "email": "initUser@gmail.com", "password": "initUser" }));
-
-        // return true;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                localStorage.removeItem('auth-token');
+                localStorage.setItem('auth-token', xmlhttp.response);
+                document.location.href = "/";
+                alert("login success! token was saved: " + xmlhttp.response);
+            } else if (xmlhttp.status == 400) {
+                alert(xmlhttp.response);
+            }
+        }
     }
-    else {
-        alert("Login was unsuccessful, please check your username and password");
-        return false;
-    }
+    var theUrl = "/api/user/login";
+    xmlhttp.open("POST", theUrl, false);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({ "email": un, "password": pw }));
 }
