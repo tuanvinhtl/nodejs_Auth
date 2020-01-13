d17d751cfa9420f3d9ae8ff96cf825c59a21aa81
Tuan Vinh <tuanvinhtl@gmail.com>
2020-01-13 23:34:35 +0700

    commit

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,14 +2,15 @@ const jwt = require("jsonwebtoken");
 const path = require('path');
 
 module.exports = (req, res, next) => {
-  const token = req.header("auth-token");
-  if (!token) {
+  const token = req.header("Authorization");
+  const bearer = token.replace(/^bearer\s/, '');
+  if (!bearer) {
     res.sendFile(path.join(__dirname + '/render-page/login.html'));
     return res;
   };
 
   try {
-    const verifyed = jwt.verify(token, process.env.TOKEN_SECRET);
+    const verifyed = jwt.verify(bearer, process.env.TOKEN_SECRET);
     req.user = verifyed;
     next();
   } catch (error) {
